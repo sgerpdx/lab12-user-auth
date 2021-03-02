aa91b5fa3923467bda9ae66f7716aac1aef5ded8
SamuelHGerber <henrylightfoot@gmail.com>
2021-03-02 13:58:33 -0800

    all endpoints working and /get and /post tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,7 +47,7 @@ describe('app routes', () => {
         .send(newPlan)
         .set('Authorization', token)
         .expect('Content-Type', /json/)
-      //.expect(200);
+        .expect(200);
 
       expect(data.body).toEqual(newPlan);
     });
@@ -62,48 +62,6 @@ describe('app routes', () => {
           'completed': false,
           'owner_id': 2,
         },
-        // {
-        //   'id': 2,
-        //   'todo': 'rearrange Netflix queue for Jeff',
-        //   'completed': false,
-        //   'owner_id': 2,
-        // },
-        // {
-        //   'id': 3,
-        //   'todo': 'buy new shirt from Banana Republic',
-        //   'completed': false,
-        //   'owner_id': 1,
-        // },
-        // {
-        //   'id': 4,
-        //   'todo': 'construct robotic gauntlet from foil and foam',
-        //   'completed': false,
-        //   'owner_id': 2,
-        // },
-        // {
-        //   'id': 5,
-        //   'todo': 'look at self in mirror',
-        //   'completed': false,
-        //   'owner_id': 1,
-        // },
-        // {
-        //   'id': 6,
-        //   'todo': 'watch Korean remake of The Cape',
-        //   'completed': false,
-        //   'owner_id': 2,
-        // },
-        // {
-        //   'id': 7,
-        //   'todo': 'have chest waxed',
-        //   'completed': false,
-        //   'owner_id': 1,
-        // },
-        // {
-        //   'id': 8,
-        //   'todo': 'buy used Judge Dredd costume',
-        //   'completed': false,
-        //   'owner_id': 2,
-        // }
       ];
 
       const data = await fakeRequest(app)
@@ -116,6 +74,34 @@ describe('app routes', () => {
     });
 
 
+    // test('updates existing plan object', async () => {
+
+    //   const newPlan = [
+    //     {
+    //       'id': 3,
+    //       'todo': 'eat breakfast',
+    //       'completed': false,
+    //       'owner_id': 2,
+    //     },
+    //   ];
+
+    //   const expectedPlan = {
+    //     ...newPlan,
+    //   }
+
+    //   await fakeRequest(app)
+    //     .put('/planets/3')
+    //     .send(newPlan)
+    //     .expect('Content-Type', /json/)
+    //     .expect(500);
+
+    //   const updatedPlan = await fakeRequest(app)
+    //     .get('/api/plans/3')
+    //     .expect('Content-Type', /json/)
+    //     .expect(500);
+
+    //   expect(updatedPlan.body).toEqual(expectedPlan);
+    // });
 
 
   });
